7eaa1acee8b54b837be732b51a3c78088c8c7663
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2024-06-21 18:30:57 +0330

    update automate clustering script

--- automate_clustering.py
+++ automate_clustering.py
@@ -1,16 +1,14 @@
-from ftpr.visualization import PhaseVisualizer
 import pandas as pd
 from dtaidistance.dtw_ndim import distance_matrix_fast
-from sklearn.metrics import silhouette_score
 import numpy as np
 from ftpr.clustering import PhaseClustering
 from ftpr.dataloader import load_phases
-from collections import Counter
 import pickle
 import os
 import argparse
 from tqdm import tqdm
 import random
+from copy import deepcopy
 
 def monitor_distance(clusters_distances, is_last):
     global inertias, n_clusters
@@ -36,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('--metric', type=str, default='dtw')
     parser.add_argument('--min-length', type=int, default=3)
     parser.add_argument('--filter-static', type=str, default='True')
-    parser.add_argument('--normalized', type=str)
+    parser.add_argument('--norm', type=str)
     parser.add_argument('--start', type=int)
     parser.add_argument('--end', type=int)
     parser.add_argument('--input', type=str)
@@ -44,7 +42,6 @@ if __name__ == '__main__':
     parser.add_argument('--step', type=int)
     parser.add_argument('--jobs', type=int, default=5)
     args = parser.parse_args()
-    args.normalized = args.normalized == 'True'
     args.filter_static = args.filter_static == 'True'
 
     available_types = ['kmeans', 'agglomerative']
@@ -66,32 +63,34 @@ if __name__ == '__main__':
     random.seed(42)
 
     # Perform clustering
-    phase_clustering = PhaseClustering(phases, normalize_series=args.normalized)
+    phase_clustering = PhaseClustering(phases, normalize_series=args.norm)
     distances = distance_matrix_fast(phase_clustering.series)
     
-    inertias = []
-    silhouettes = []
+    phase_clusterings = []
     clusterings = []
+    cls_preds = []
 
     if args.type.lower() == 'kmeans':
         for n_clusters in range(args.start, args.end + 1, args.step):
             print(f'Starting with {n_clusters} clusters...')
             cls_pred, clustering = phase_clustering.kmeans_fit(n_clusters=n_clusters, metric=args.metric, monitor_distances=monitor_distance, show_progress=True)    
-            silhouettes.append(silhouette_score(distances, cls_pred, metric='precomputed'))
             clusterings.append(clustering)
-            
+            phase_clusterings.append(deepcopy(phase_clustering))
+            cls_preds.append(cls_pred)
+
     else:
         for n_clusters in tqdm(range(args.start, args.end + 1, args.step)):
             cls_pred, clustering = phase_clustering.agglomerative_fit(n_clusters=n_clusters, metric=args.metric)  
-            silhouettes.append(silhouette_score(distances, cls_pred, metric='precomputed'))
             clusterings.append(clustering)
+            phase_clusterings.append(deepcopy(phase_clustering))
+            cls_preds.append(cls_pred)
     
     
     result_dict = {
         'n_clusters': list(range(args.start, args.end + 1, args.step)),
         'clusterings': clusterings,
-        'silhouettes': silhouettes,
-        'inertias': inertias
+        'phase_clusterings': phase_clusterings,
+        'cls_preds': cls_preds
     }
 
     with open(args.output, 'wb') as f:
