685f04efdc2529212c82cf584f68648e24510ed5
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2024-06-26 21:56:20 +0330

    improve preprocessing

--- ftpr/src/ftpr/preprocessing.py
+++ ftpr/src/ftpr/preprocessing.py
@@ -5,7 +5,7 @@ import numpy as np
 from copy import deepcopy
 
 class PhaseExtractor:
-    DELAY_EVENTS = ['foul won', 'foul committed', 'injury stoppage', 'referee ball-drop', 'half end', 'half start']
+    DELAY_EVENTS = ['foul won', 'foul committed', 'injury stoppage', 'referee ball-drop', 'half end', 'half start', 'offside']
 
     HAVING_BALL_EVENTS = [
         'carry', 'ball recovery', 'goal keeper', 'clearance',
@@ -15,7 +15,7 @@ class PhaseExtractor:
     # ball_receipt_outcome: Incomplete
     # dribble outcome: Incomplete
     
-    LOSING_BALL_EVENTS = ['dispossessed', 'offside', 'error', 'miscontrol']
+    LOSING_BALL_EVENTS = ['dispossessed', 'error', 'miscontrol']
 
 
     IGNORE_EVENTS = [
@@ -90,52 +90,28 @@ class PhaseExtractor:
             start_index = self._find_next_event(match_df, 2, team_name)
             current_index = start_index
 
-            end_of_phase = False
             while current_index < len(match_df):
                 event = match_df.iloc[current_index]['type'].lower()
                 team =  match_df.iloc[current_index]['team']
-                # if the event is in having_ball_events
-                
-                known_event = False
-                # print('index:', current_index)
-                # print(team, event)
-                if end_of_phase or team != team_name or event in PhaseExtractor.LOSING_BALL_EVENTS or event in PhaseExtractor.DELAY_EVENTS:
-                    # Extract the series
-                    # print('Losing the ball')
+
+                if event in PhaseExtractor.HAVING_BALL_EVENTS and team == team_name and (event != 'dribble' or  match_df.iloc[current_index]['dribble_outcome'] != 'Incomplete'):
+                    current_index += 1
+
+                elif event in PhaseExtractor.HAVING_BALL_EVENTS or event in PhaseExtractor.LOSING_BALL_EVENTS or event in PhaseExtractor.DELAY_EVENTS:
                     if start_index != current_index:
                         phase_df = self._handle_locations(match_df.iloc[start_index:current_index])
                         phase_df = phase_df.assign(phase_id=phase_id)
                         phase_id += 1
                         result_df = pd.concat((result_df, phase_df))
-                    known_event = True
-                    end_of_phase = False
-                    # print('end_of_phase:', False)
-                    # clear the current series
+
                     start_index = self._find_next_event(match_df, current_index + 1, team_name)
-                    current_index = start_index  
-                if not end_of_phase and event in PhaseExtractor.HAVING_BALL_EVENTS:
-                    # print('Having the ball')
-                    if event == 'dribble' and  match_df.iloc[current_index]['dribble_outcome'] == 'Incomplete':
-                        end_of_phase = True                    
-                        # print('end_of_phase', True)
-                    else:
-                        current_index += 1
-                    known_event = True
-                if not known_event:
+                    current_index = start_index
+
+                else:
                     raise NotImplementedError(f'This type of event ({event}) is not implemented! (Index: {match_df.iloc[current_index]["index"]}, file: {df_dir})')
+                
 
-        # Ball receipt should be ignored if the next event is not in the same location and the previous event’s location must be fixed
-        # new_df = result_df.copy()
-        # new_df.insert(len(new_df.columns), 'keep', True)
-        # for i in range(1, len(new_df) - 1):
-        #     row = new_df.iloc[i]
-        #     next_row = new_df.iloc[i + 1]
-        #     if row['type'] == 'ball receipt*' and row['location'] != next_row['location']:
-        #         new_df.iat[i, len(new_df.columns)] = False
-        #         prev_row_event = new_df.iloc[i - 1]['type']
-        #         if prev_row_event in ['pass', 'dribble']
-
-        result_df=result_df.reset_index(drop=True)
+        result_df = result_df.reset_index(drop=True)
 
         if output_dir:
             # create output directory if not exists
