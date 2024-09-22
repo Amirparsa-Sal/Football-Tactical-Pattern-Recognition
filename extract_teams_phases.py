9dc5e0d6c41068b110bf08dc70b40181e011c9eb
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2024-09-23 01:38:06 +0330

    add argparse to extract phases script

--- extract_teams_phases.py
+++ extract_teams_phases.py
@@ -1,12 +1,24 @@
 import pandas as pd
 from ftpr.preprocessing import PhaseExtractor
 import os
+import argparse
 
-matches_df = pd.read_csv('./data/matches.csv')
+parser = argparse.ArgumentParser()
+parser.add_argument('--matches', type=str, default='./data/matches.csv')
+parser.add_argument('--events', type=str, default='./data/events_simplified')
+args = parser.parse_args()
+
+if not os.path.exists(args.matches):
+    raise FileNotFoundError('Matches csv not found!')
+
+if not os.path.exists(args.events):
+    raise FileNotFoundError('Events directory not found!')
+
+matches_df = pd.read_csv(args.matches)
 
 teams = set(matches_df['home_team'])
 
-ext = PhaseExtractor(matches_df, './data/events_simplified')
+ext = PhaseExtractor(matches_df, args.events)
 
 for team in teams:
     if os.path.exists(f'./data/team_phases/{team}.csv'):
