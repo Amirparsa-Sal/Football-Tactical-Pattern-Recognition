0504b34ea840eb827ba4668d5c0f5516ca56e791
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2024-09-25 17:41:03 +0330

    add output-dir to extract phases script

--- extract_teams_phases.py
+++ extract_teams_phases.py
@@ -4,8 +4,9 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--matches', type=str, default='./data/matches.csv')
-parser.add_argument('--events', type=str, default='./data/events_simplified')
+parser.add_argument('--matches', type=str, default=os.path.join('data', 'matches.csv'))
+parser.add_argument('--events', type=str, default=os.path.join('data', 'events_simplified'))
+parser.add_argument('--output-dir', type=str, default=os.path.join('data', 'team_phases'))
 args = parser.parse_args()
 
 if not os.path.exists(args.matches):
@@ -21,8 +22,8 @@ teams = set(matches_df['home_team'])
 ext = PhaseExtractor(matches_df, args.events)
 
 for team in teams:
-    if os.path.exists(f'./data/team_phases/{team}.csv'):
+    if os.path.exists(os.path.join(args.output_dir, f'{team}.csv')):
         print(team, 'Passed!')
     else:
         print(f'Extracting {team} data...')
-        ext.extract_phases(team, output_dir='./data/team_phases')
+        ext.extract_phases(team, output_dir=args.output_dir)
